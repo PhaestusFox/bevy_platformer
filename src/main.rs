c3adbd45b13841b443bc5e2c379ff4709a33d439
PhaestusFox <phaestusfox@gmail.com>
2023-01-22 22:43:20 +0800

    spawn camera

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,12 @@ use bevy::prelude::*;
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
+    .add_startup_system(spawn_cam)
     .run()
+}
+
+fn spawn_cam(
+    mut commands: Commands,
+) {
+    commands.spawn(Camera2dBundle::default());
 }

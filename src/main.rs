61cf50c6dbcb5511f1ccc45fca7233f56898cef4
PhaestusFox <phaestusfox@gmail.com>
2023-01-22 23:01:28 +0800

    animate character

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main() {
     .add_plugin(bevy_editor_pls::prelude::EditorPlugin)
     .add_startup_system(spawn_cam)
     .add_startup_system(spawn_player)
+    .add_system(animate_sprite)
+    .register_type::<TextureAtlasSprite>()
     .run()
 }
 
@@ -26,11 +28,42 @@ fn spawn_player(
     let atlas = TextureAtlas::from_grid(
         asset_server.load("Main Characters/Mask Dude/Idle (32x32).png"),
         Vec2::splat(32.),
-        1, 11, None, None);
+        11, 1, None, None);
     commands.spawn((SpriteSheetBundle {
         texture_atlas: texture_atlas.add(atlas),
         sprite: TextureAtlasSprite {index: 0, ..Default::default()},
         ..Default::default()
-    }, Player));
+    }, Player,
+    SpriteAnimation {
+        len: 11,
+        frame_time: 1./10.,
+    },
+    FrameTime(0.0)
+    ));
 }
 
+#[derive(Component)]
+struct SpriteAnimation {
+    len: usize,
+    frame_time: f32,
+}
+
+#[derive(Component)]
+struct FrameTime(f32);
+
+fn animate_sprite(
+    mut query: Query<(&mut TextureAtlasSprite, &SpriteAnimation, &mut FrameTime)>,
+    time: Res<Time>,
+) {
+    for (mut sprite, animation, mut frame_time) in query.iter_mut() {
+        frame_time.0 += time.delta_seconds();
+        if frame_time.0 > animation.frame_time {
+            let frames = (frame_time.0 / animation.frame_time) as usize;
+            sprite.index += frames;
+            if sprite.index >= animation.len {
+                sprite.index %= animation.len;
+            }
+            frame_time.0 -= animation.frame_time;
+        }
+    }
+}

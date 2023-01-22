c6dbcc53640a5e74bcf42dce5257ecc027932f5e
PhaestusFox <phaestusfox@gmail.com>
2023-01-22 22:43:35 +0800

    spawn player

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
     .add_startup_system(spawn_cam)
+    .add_startup_system(spawn_player)
     .run()
 }
 
@@ -12,3 +13,23 @@ fn spawn_cam(
 ) {
     commands.spawn(Camera2dBundle::default());
 }
+
+#[derive(Component)]
+struct Player;
+
+fn spawn_player(
+    mut commands: Commands,
+    mut texture_atlas: ResMut<Assets<TextureAtlas>>,
+    asset_server: Res<AssetServer>,
+) {
+    let atlas = TextureAtlas::from_grid(
+        asset_server.load("Main Characters/Mask Dude/Idle (32x32).png"),
+        Vec2::splat(32.),
+        1, 11, None, None);
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: texture_atlas.add(atlas),
+        sprite: TextureAtlasSprite {index: 0, ..Default::default()},
+        ..Default::default()
+    });
+}
+

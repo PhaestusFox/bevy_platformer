9e25eecfc65b4d912375f61fddc4b3e12b5ee223
PhaestusFox <phaestusfox@gmail.com>
2023-02-19 11:19:54 +0800

    added auto_ghost
    a ghost will be spawned after 2.5 seconds after the player moves more then 8 pixels from the origin.
    the trail is cleared every frame if the player is at the origin to stop players making long stationary trails before going to collect collectables

--- src/ghost.rs
+++ src/ghost.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, ecs::query::QuerySingleError};
 
 use crate::{
     animation::{Animation, Animations},
@@ -24,7 +24,8 @@ impl Plugin for GhostPlugin {
             .add_system(test_ghost)
             .add_event::<GhostEvents>()
             .add_system(handle_ghost_event)
-            .add_system(kill_player);
+            .add_system(kill_player)
+            .add_system(auto_ghost);
     }
 }
 
@@ -215,5 +216,41 @@ fn kill_player(
             *pos = Transform::IDENTITY;
             *vel = Velocity::zero();
         };
+    }
+}
+
+struct GhostTimer(Timer);
+impl Default for GhostTimer {
+    fn default() -> Self {
+        GhostTimer(Timer::from_seconds(2.5, TimerMode::Once))
+    }
+}
+
+fn auto_ghost(
+    has_ghost: Query<&Ghost>,
+    player: Query<&Transform, With<RealPlayer>>,
+    mut count_down: Local<GhostTimer>,
+    mut events: ParamSet<(EventReader<GhostEvents>, EventWriter<GhostEvents>)>,
+    time: Res<Time>,
+) {
+    for event in events.p0().iter() {
+        match event {
+            GhostEvents::ClearGhosts => {
+                count_down.0.reset();
+            },
+            _ => {}
+        }
+    }
+    if let Err(QuerySingleError::NoEntities(_)) = has_ghost.get_single() {
+        let player = player.single();
+        if player.translation.distance(Vec3::ZERO) < 8. && !count_down.0.finished() {
+            events.p1().send(GhostEvents::ClearTrail);
+            count_down.0.reset();
+            return;
+        }
+        count_down.0.tick(time.delta());
+        if count_down.0.finished() {
+            events.p1().send(GhostEvents::SpawnGhost);
+        }
     }
 }

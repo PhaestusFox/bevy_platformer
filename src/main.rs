a844bc92a8e7961d10ce27db61cebf98e165de68
PhaestusFox <phaestusfox@gmail.com>
2023-02-24 10:20:56 +0800

    added a main menu

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,12 @@ mod ghost;
 mod player;
 mod user_input;
 mod map;
+mod menu;
 
 use animation::*;
 use player::*;
 use map::*;
+
 
 fn main() {
     App::new()
@@ -21,7 +23,6 @@ fn main() {
         .add_plugin(PhoxAnimationPlugin)
         .add_startup_system(spawn_cam)
         .add_startup_system(spawn_map)
-        .add_system(update_map)
         .add_system(get_collectable)
         .register_type::<TextureAtlasSprite>()
         .add_plugin(InputManagerPlugin::<user_input::PlayerInput>::default())
@@ -40,6 +41,8 @@ fn main() {
         .add_plugin(MapPlugin)
         .add_plugin(ghost::GhostPlugin)
         .insert_resource(Score(0))
+        .add_state(GameState::Menu)
+        .add_plugin(menu::MenuPlugin)
         .run()
 }
 
@@ -54,116 +57,7 @@ fn spawn_map(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    commands.insert_resource(CurrentLevel(asset_server.load("Levels/test.lvl.ron"), false));
-}
-
-fn update_map(
-    mut map_event: EventWriter<MapEvent>,
-    levels: Res<Assets<Level>>,
-    mut current_level: ResMut<CurrentLevel>,
-) {
-    if current_level.1 {
-        return;
-    }
-    let Some(level) = levels.get(&current_level.0) else {return;};
-    for obj in level.objects.iter() {
-        map_event.send(MapEvent::Spawn(MapObject::clone(obj.as_ref())))
-    }
-    current_level.1 = true;
-    // map_event.send(MapEvent::spawn(MapBox {
-    //     offset: IVec3 { x: -6, y: -1, z: 1 },
-    //     width: 13,
-    //     hight: 1,
-    //     material: TerrainMaterial::Gold,
-    // }));
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: 7, y: 1, z: 1 },
-    //     width: 2,
-    //     hight: 2,
-    //     material: TerrainMaterial::Gold,
-    // })));
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: 7, y: 1, z: 1 },
-    //     width: 1,
-    //     hight: 1,
-    //     material: TerrainMaterial::Clay,
-    // })));
-    // for i in 0..5 {
-    //     map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //         offset: IVec3 {
-    //             x: -11,
-    //             y: 4-i,
-    //             z: 1,
-    //         },
-    //         width: 1 + i,
-    //         hight: 1,
-    //         material: TerrainMaterial::Gold,
-    //     })));
-    // }
-
-    // for i in 0..5 {
-    //     map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //         offset: IVec3 {
-    //             x: i * 2,
-    //             y: 15,
-    //             z: 1,
-    //         },
-    //         width: 1,
-    //         hight: 1,
-    //         material: TerrainMaterial::Brick,
-    //     })));
-    // }
-
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: -5, y: 10, z: 1 },
-    //     width: 1,
-    //     hight: 4,
-    //     material: TerrainMaterial::Gold,
-    // })));
-
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: -6, y: 9, z: 1 },
-    //     width: 1,
-    //     hight: 5,
-    //     material: TerrainMaterial::Gold,
-    // })));
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: -6, y: 9, z: 1 },
-    //     width: 1,
-    //     hight: 1,
-    //     material: TerrainMaterial::Clay,
-    // })));
-
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: -10, y: 6, z: 1 },
-    //     width: 2,
-    //     hight: 2,
-    //     material: TerrainMaterial::Gold,
-    // })));
-
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: -2, y: 7, z: 1 },
-    //     width: 5,
-    //     hight: 1,
-    //     material: TerrainMaterial::Copper,
-    // })));
-
-    // map_event.send(MapEvent::Spawn(Box::new(MapBox {
-    //     offset: IVec3 { x: -2, y: 8, z: 1 },
-    //     width: 4,
-    //     hight: 1,
-    //     material: TerrainMaterial::Iron,
-    // })));
-
-    // map_event.send(MapEvent::spawn(Collectable {
-    //     collectable_type: CollectableType::Strawberry,
-    //     spawn_type: SpawnType::Fixed(IVec2::new(2, 1)),
-    // }));
-
-    // map_event.send(MapEvent::spawn(Collectable {
-    //     collectable_type: CollectableType::Bananan,
-    //     spawn_type: SpawnType::RandomRange(IVec2::new(-10, 0), IVec2::new(10, 20)),
-    // }));
+    commands.insert_resource(LoadedLevel(asset_server.load("Levels/test.lvl.ron")));
 }
 
 fn get_collectable(
@@ -197,3 +91,11 @@ fn get_collectable(
 
 #[derive(Resource)]
 struct Score(usize);
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, Component)]
+enum GameState {
+    Play,
+    Menu,
+    InputLevelBase64,
+    InputLevelName,
+}
